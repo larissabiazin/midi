a1d8af01aac9c989368d98b1ada6029741b9cb82
larissabiazin <108530433+larissabiazin@users.noreply.github.com>
2023-09-20 12:23:12 -0300

    s

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-
+//tecla Pom
 function tocaSomPom () {
 document.querySelector("#Som_Tecla_Pom").play();
 }
 
 document.querySelector(".tecla_pom").onclick = tocaSomPom; 
 
-
+//tecla clap
+function tocaSomClap () {
+    document.querySelector("#Som_Tecla_Clap").play();
+    }
+    
+    document.querySelector(".tecla_clap").onclick = tocaSomClap; 
+    
